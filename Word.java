2b40e03be700132bf0078e60cae99b2012df6c04
QuangBinhDinh <g4.terminator@gmail.com>
2018-09-25 14:19:06 +0700

    Code co ban

---
+++ Word.java
@@ -0,0 +1,19 @@
+package Dictionary;
+
+/**
+ *
+ * @author Win10
+ */
+public class Word {
+    private String target;
+    private String explain;
+    public Word(String target, String explain){
+        this.target = target; this.explain = explain;
+    }
+    public void setTarget(String target){
+        this.target = target;
+    }
+    public void setExpalin(String explain){
+        this.explain = explain;
+    }
+}
